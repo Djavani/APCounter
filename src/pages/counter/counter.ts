38f5de7d45ee4387375f1a13db4983563f428653
djavani <djavanirgomes@gmail.com>
2018-04-21 00:22:01 -0300

    implementando contagem

--- src/pages/counter/counter.ts
+++ src/pages/counter/counter.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 import { LocalStorageService } from './../../app/services/localStorageService';
 import { Storage } from '@ionic/storage';
-
+import { ToastController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -15,36 +15,101 @@ import { Storage } from '@ionic/storage';
 })
 export class CounterPage {
 
+  public valor: number;
   public valorPA : any;
   public isOrderAsc : any;
+  public isDisabledButtonUp = false;
+  public isDisabledButtonDown = false;
 
-  constructor(  
-    public navCtrl: NavController, 
-    public navParams: NavParams,    
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private alertCtrl: AlertController,
+    private toastCtrl: ToastController,
     private storageService: LocalStorageService,
     private storage: Storage) {
-
-      this.getOrderAsc();
-      this.getValorPA();
   }
 
-  ionViewDidLoad() { 
-    this.getOrderAsc();
-    this.getValorPA();
+  ionViewDidLoad() {
+    this.recuperarConfig();
   }
 
-  getValorPA(){        
+  recuperarConfig() {
+    this.storage.get('isOrderAsc').then(value => {
+      this.isOrderAsc = value;
+
+    })
     this.storage.get('valorPA').then(value => {
+      if(this.isOrderAsc){
+        this.valor = 0;
+      }else {
+        this.valor = value;
+      }
       this.valorPA = value;
     })
-    
+    this.isDisabledButtonUp = false;
+    this.isDisabledButtonDown = false;
   }
-  
-  getOrderAsc() {  
-    this.storage.get('isOrderAsc').then(value => {
-    this.isOrderAsc = value
-    })
-   
+
+  usarPontoDeAcao(acao: string) {
+    // crescente e subir
+    if(this.isOrderAsc && acao == "up") {
+      this.isDisabledButtonDown = false;
+      if(this.valor < this.valorPA){
+        this.valor++;
+      }else {
+        this.presentToast("Pontos de ação ESGOTADOS!");
+        this.isDisabledButtonUp = true;
+      }
+    }
+
+    //crescente e descer
+    if(this.isOrderAsc && acao == "down") {
+      this.isDisabledButtonUp = false;
+      if(this.valor >= 1 ){
+        this.valor--;
+      }else {
+        this.presentToast("Pontos de ação ZERADOS!");
+        this.isDisabledButtonDown = true;
+      }
+    }
+
+    //decrescente > descendo
+    if(!this.isOrderAsc && acao == "up") {
+      this.isDisabledButtonDown = false;
+      if(this.valor >0){
+        this.valor--;
+      }else {
+        this.presentToast("Pontos de ação ZERADOS!");
+        this.isDisabledButtonUp = true;
+      }
+    }
+
+    //decrescendo > subindo
+    if(!this.isOrderAsc && acao == "down") {
+      this.isDisabledButtonUp = false;
+      if(this.valor < this.valorPA){
+        this.valor++;
+      }else {
+        this.presentToast("Pontos de ação ZERADOS!");
+        this.isDisabledButtonDown = true;
+      }
+    }
+
+  }
+
+  presentToast(message: string) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000,
+      position: 'top'
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
   }
 
 }
